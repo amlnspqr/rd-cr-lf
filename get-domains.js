7ec627b970f79610ea59d31be6e5c2871d97ca38
amlnspqr <amlnspqr@ya.ru>
2019-02-05 01:48:59 +0300

    init

---
+++ get-domains.js
@@ -0,0 +1,119 @@
+// ==UserScript==
+// @name		 Get Domains
+// @namespace	 *
+// @description Get Domains
+// @include		 *
+// @version	1.0
+// @grant		none
+// ==/UserScript==
+
+window.addEventListener
+(
+	'keydown', 
+	function (e)
+	{
+		if (e.altKey || e.modifiers)
+		{
+			if (e.keyCode == 68)
+				getDomains ()
+			
+			if (e.keyCode == 70)
+			{
+				var sDomain = prompt ('Enter domain', location.host)
+				
+				sDomain && openScanners (sDomain)
+			}
+		}
+	},
+	false
+)
+
+window.addEventListener
+(
+	'DOMContentLoaded', 
+	function (e)
+	{
+		if (location.host == 'dnsdumpster.com' && location.hash && document.getElementById ('regularInput'))
+		{
+			var input = document.getElementById ('regularInput')
+			
+			input.value = location.hash.slice (1)
+			input.form.submit ()
+		}
+	}, 
+	false
+)
+
+function openScanners (sDomain)
+{
+	window.open ('https://crt.sh/?q=%.' + sDomain)
+	window.open ('https://www.virustotal.com/en/domain/' + sDomain + '/information/')
+	window.open ('https://dnsdumpster.com/#' + sDomain)
+	window.open ('https://www.google.com/search?q=site:*.' + sDomain + ' -www')
+}
+
+function getDomain ()
+{
+	try
+	{
+		var domain = location.search.match (/%2?5?\.(.+)/) [1]		//	crt
+	}
+	catch (e)
+	{
+		try
+		{
+			var domain = location.pathname.match (/domain\/(.+)\/information/) [1]		//	virustotal
+		}
+		catch (e)
+		{
+			try
+			{
+				var domain = document.body.innerHTML.match (/Showing results for (.+)/) [1].replace (/<.+?>/g, '')		//	dnsdumpster
+			}
+			catch (e)
+			{
+				try
+				{
+					var domain = document.getElementsByName ('q') [0].value.match (/site:(.+?)(\s|$)/) [1].replace ('*.', '')		//	google
+				}
+				catch (e)
+				{
+					var domain = ''
+				}
+			}
+		}
+	}
+	
+	return domain
+}
+
+function getDomains ()
+{
+	var sDomain = getDomain ()
+	
+	if (!sDomain)
+	{
+		alert ('Domain not found')
+		
+		return 0
+	}
+	
+	var rDomains = new RegExp ('([\\w-]+\\.)+' + sDomain.replace (/\./g, '\\.'), 'g')
+	
+	var aDomains = unique (document.body.innerHTML.match (rDomains).map (function (i) {return i.replace (/^www\./, '')})).join ('\n').split (/([\s\S]{10000})/)
+	
+	aDomains.forEach (function (i) {i && alert (i)})
+}
+
+function unique (arr)
+{
+	var obj = {}
+	
+	for (var i = 0; i < arr.length; i++)
+	{
+		var str = arr [i].toLowerCase ()
+		obj [str] = true
+	}
+	
+	return Object.keys (obj)
+}
